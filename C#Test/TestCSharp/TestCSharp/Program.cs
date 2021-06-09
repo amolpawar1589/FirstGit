a88ea5e1b5d61f380419e6ae518169cb3b87be81
Amol <amolpawar1589@gmail.com>
2021-06-09 11:20:01 +0530

    add another object

--- C#Test/TestCSharp/TestCSharp/Program.cs
+++ C#Test/TestCSharp/TestCSharp/Program.cs
@@ -7,7 +7,7 @@ namespace TestCSharp
         //constructor-Implict and explicit 
         //implicit -which is declare by compiler when developer is not declare anything
         //explicit is 
-        int i;string s;bool b;
+        int i; string s; bool b;
 
 
         static void Main(string[] args)
@@ -17,6 +17,7 @@ namespace TestCSharp
             Program p = new Program();
             Console.WriteLine(p.i);
             Console.WriteLine(p.s);
+            Console.WriteLine(p.b);
             Console.WriteLine(p.b);
             Console.WriteLine(p.b);
             Console.WriteLine(p.b);
